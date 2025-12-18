6c8383b932c87fc5b18a871c5daaead772eb6f5d
Jay963854 <jay@gmail.com>
2025-12-18 18:20:06 +0100

    Updates

--- src/lib/videoThumbnails.ts
+++ src/lib/videoThumbnails.ts
@@ -1,3 +1,4 @@
 export const videoThumbnails: Record<string, string> = {
   fwSDf9Me2OU: "/thumbnails/v1.jpg",
+  peRWL9CIsCk: "/thumbnails/m1.jpg",
 };
--- src/lib/youtubeThumbnails.ts
+++ src/lib/youtubeThumbnails.ts
@@ -1,3 +1,4 @@
 export const youtubeThumbnails: Record<string, string> = {
   fwSDf9Me2OU: "/thumbnails/v4.jpg",
+  peRWL9CIsCk: "/thumbnails/m2.jpg",
 };
